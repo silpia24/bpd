3e44bbcda126e216655ae7f7a8dcb00bbf021bf6
silpia nuruls <2412510030@student.budiluhur.ac.id>
2025-01-08 13:30:47 +0700

    no2list.py

---
+++ uas/no2list.py
@@ -0,0 +1,13 @@
+# Buat program dengan Python menggunakan tipe List dengan keluaran (output) seperti terlihat pada gambar 
+
+nilaiTK = [90, 75, 95, 85, 80]
+
+print("Mencetak semua isi list:\n", nilaiTK)
+print("===========================================")
+print("Mencetak isi list index ke-1:\n", nilaiTK[1])
+print("===========================================")
+print("Mencetak isi list index ke-1 sampai ke-3:\n", nilaiTK[1:3])
+print("===========================================")
+
+nilaiKu = nilaiTK[2]
+print("Mengambil isi list index ke-2 ke variable nilaiKu:\n nilaiKu = ", nilaiKu)
